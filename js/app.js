c6735ead91ed73b91b7cdc721e8a7006ac9cd0ce
raghidairsheid <raghidair@gmail.com>
2021-04-25 21:47:31 +0300

    up new

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-'use strict';
+
 
 let typeN = prompt('Do you love Space? yes or no');
 console.log(typeN);
@@ -15,16 +15,14 @@ document.writeln('sum= ' , sum);
 let NF = prompt('please enter type: (Space of the moon ,Intergalactic space)?');
 
 
-document.writeln()
-
 if(NF == 'Space of the moon')
-    {
-      let image1 ='<img src="https://th.bing.com/th/id/OIP.Sd2GZNfifzyv2LvnPSAPTwHaH4?w=177&h=189&c=7&o=5&pid=1.7"/>' ;
-      document.writeln('<p>This is Space of the moon</p>' + image1  + "<br>");
-    }
-    else if(NF == 'Intergalactic space'){
-        var image2 = '<img src="https://th.bing.com/th/id/OIP.6rrlxMr4EMKe0v16gNvjWgHaEo?w=297&h=185&c=7&o=5&pid=1.7" /> ';
-        document.writeln("<p>This is Intergalactic space </p>" + image2+ "<br>");
-    }
-    console.log(NF);
+{
+    let image1 ='<img src="https://th.bing.com/th/id/OIP.Sd2GZNfifzyv2LvnPSAPTwHaH4?w=177&h=189&c=7&o=5&pid=1.7"/>' ;
+    document.writeln('<p>This is Space of the moon</p>' + image1  + "<br>");
+}
+else if(NF == 'Intergalactic space'){
+    var image2 = '<img src="https://th.bing.com/th/id/OIP.6rrlxMr4EMKe0v16gNvjWgHaEo?w=297&h=185&c=7&o=5&pid=1.7" /> ';
+    document.writeln("<p>This is Intergalactic space </p>" + image2+ "<br>");
+}
+console.log(NF);
   
