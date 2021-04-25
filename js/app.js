9d5b3c83bcb5dcdef848dff04cab3a2017f6d7ef
raghidairsheid <raghidair@gmail.com>
2021-04-25 21:44:28 +0300

    upfile

--- js/app.js
+++ js/app.js
@@ -10,6 +10,7 @@ let stu2 = parseInt(prompt('please enter number of student 2:'));
 
 let sum = stu1 + stu2;
 console.log('sum= ' , sum);
+document.writeln('sum= ' , sum);
 
 let NF = prompt('please enter type: (Space of the moon ,Intergalactic space)?');
 
@@ -19,11 +20,11 @@ document.writeln()
 if(NF == 'Space of the moon')
     {
       let image1 ='<img src="https://th.bing.com/th/id/OIP.Sd2GZNfifzyv2LvnPSAPTwHaH4?w=177&h=189&c=7&o=5&pid=1.7"/>' ;
-      document.writeln('This is Space of the moon' + image1  + "<br>");
+      document.writeln('<p>This is Space of the moon</p>' + image1  + "<br>");
     }
     else if(NF == 'Intergalactic space'){
         var image2 = '<img src="https://th.bing.com/th/id/OIP.6rrlxMr4EMKe0v16gNvjWgHaEo?w=297&h=185&c=7&o=5&pid=1.7" /> ';
-        document.writeln("This is Intergalactic spacebr" + image2+ "<br>");
+        document.writeln("<p>This is Intergalactic space </p>" + image2+ "<br>");
     }
     console.log(NF);
   
