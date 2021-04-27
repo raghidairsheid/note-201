fcfd70b4291b7acc665c85f261ac6f3d330793f7
raghidairsheid <raghidair@gmail.com>
2021-04-27 16:40:17 +0300

    upfile

--- js/app.js
+++ js/app.js
@@ -2,17 +2,20 @@
 
 let typeN = prompt('Do you love Space? yes or no');
 console.log(typeN);
-
-
+alert('the answer is :'+ typeN);
 
 let stu1= parseInt(prompt('please enter number of student 1:'));
+console.log('please enter number of student 1:'+ stu1);
 let stu2 = parseInt(prompt('please enter number of student 2:'));
+console.log('please enter number of student 2:'+stu2);
+alert('the number of student 1 is: ' + stu1+ ' the number of student 2 is: ' + stu2);
 
 let sum = stu1 + stu2;
 console.log('sum= ' , sum);
 document.writeln('sum= ' , sum);
 
 let NF = prompt('please enter type: (Space of the moon ,Intergalactic space)?');
+console.log('(Space of the moon ,Intergalactic space)? '+ NF);
 
 
 if(NF == 'Space of the moon')
@@ -21,8 +24,8 @@ if(NF == 'Space of the moon')
     document.writeln('<p>This is Space of the moon</p>' + image1  + "<br>");
 }
 else if(NF == 'Intergalactic space'){
-    var image2 = '<img src="https://th.bing.com/th/id/OIP.6rrlxMr4EMKe0v16gNvjWgHaEo?w=297&h=185&c=7&o=5&pid=1.7" /> ';
+    let image2 = '<img src="https://th.bing.com/th/id/OIP.6rrlxMr4EMKe0v16gNvjWgHaEo?w=297&h=185&c=7&o=5&pid=1.7" /> ';
     document.writeln("<p>This is Intergalactic space </p>" + image2+ "<br>");
 }
-console.log(NF);
-  
+console.log('(Space of the moon ,Intergalactic space)? '+ NF);
+alert('answer is: ' + NF); 
